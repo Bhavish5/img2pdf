0f34ff43983852687bf5e3ab2d3ec935b38a1909
Bhavish5 <bhavish7turaga@gmail.com>
2024-05-17 12:10:41 +0530

    Added requirements.txt and automatic pdf download from server

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
 from PIL import Image
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    if os.path.exists("output.pdf"):
+        os.remove("output.pdf")
     return render_template("index.html")
 
 @app.route("/handle", methods=["POST", ])
@@ -20,6 +24,11 @@ def handle():
                 #image.save(secure_filename(image.filename))
             #print(new_images)
             new_images[0].save("output.pdf", save_all=True, append_images=new_images[1:])
+            pdf_path = "output.pdf"
+            if os.path.exists(pdf_path):
+                return send_file(pdf_path, as_attachment=True)
+            else:
+                return "Images not found"
         return "Got the images"
     except:
         return "Upload failed" 
